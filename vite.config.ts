9d22e9b59cd8d8b35de0832bb5577ceb10c3c7b2
mlane <mlane@travelpassgroup.com>
2025-03-15 11:30:38 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'src/main.ts',
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-    assetsDir: '', // Ensure that assets are in the root of the dist directory
-    sourcemap: false,
-    manifest: false,
-    chunkSizeWarningLimit: 500,
+    assetsDir: 'assets', // Ensure correct asset handling
   },
-  publicDir: 'public', // Ensure Vite uses the public directory
-  esbuild: {
-    minify: false,
-    // Ensures no versioned filenames
-    minifyIdentifiers: false,
-    minifyWhitespace: false,
-    minifySyntax: false,
+  server: {
+    port: 3000,
   },
 })
