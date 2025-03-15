7d77044ae1c3e7ddcfcac127c979b4b436c2479a
mlane <mlane@travelpassgroup.com>
2025-03-15 11:26:46 -0600

    Add WebGL availability check with user warning

--- src/main.ts
+++ src/main.ts
@@ -1,261 +1,270 @@
 import './styles.css'
 import kaboom from 'kaboom'
 import { dialogueData, scaleFactor } from './constants'
-import { setCameraScale, showDialogue } from './utils'
+import { isWebGlAvailable, setCameraScale, showDialogue } from './utils'
 
-const k = kaboom({
-  canvas: document.getElementById('game') as HTMLCanvasElement,
-  debug: false, // set to false once ready for production
-  global: false,
-  touchToMouse: true,
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.createElement('canvas')
 
-k.loadSprite('map', './map.png')
+  if (!isWebGlAvailable(canvas))
+    return document.documentElement.classList.add('no-webgl')
 
-k.loadSprite('spritesheet', './spritesheet.png', {
-  anims: {
-    'idle-down': {
-      from: 0,
-      loop: true,
-      speed: 8,
-      to: 1,
-    },
-    'idle-side': {
-      from: 0,
-      loop: true,
-      speed: 8,
-      to: 1,
-    },
-    'idle-up': {
-      from: 12,
-      loop: true,
-      speed: 8,
-      to: 13,
-    },
-    'walk-down': {
-      from: 5,
-      loop: true,
-      speed: 8,
-      to: 3,
-    },
-    'walk-side': {
-      from: 8,
-      loop: true,
-      speed: 8,
-      to: 9,
-    },
-    'walk-up': {
-      from: 15,
-      loop: true,
-      speed: 8,
-      to: 17,
-    },
-  },
-  sliceX: 6,
-  sliceY: 3,
-})
+  document.body.children[0].appendChild(canvas)
 
-k.setBackground(k.Color.fromHex('#000000'))
+  const k = kaboom({
+    canvas,
+    debug: false, // set to false once ready for production
+    global: false,
+    touchToMouse: true,
+  })
 
-k.scene('main', async () => {
-  const mapData = await (await fetch('./map.json')).json()
-  const layers = mapData.layers
-  const map = k.add([k.sprite('map'), k.pos(0, 0), k.scale(scaleFactor)])
-  const player = k.make([
-    k.sprite('spritesheet', {
-      anim: 'idle-down',
-    }),
-    k.area({
-      shape: new k.Rect(k.vec2(0, 3), 10, 10),
-    }),
-    k.body(),
-    k.anchor('center'),
-    k.pos(),
-    k.scale(scaleFactor),
-    {
-      direction: 'down',
-      isInDialogue: false,
-      speed: 300,
+  k.loadSprite('map', './map.png')
+
+  k.loadSprite('spritesheet', './spritesheet.png', {
+    anims: {
+      'idle-down': {
+        from: 0,
+        loop: true,
+        speed: 8,
+        to: 1,
+      },
+      'idle-side': {
+        from: 0,
+        loop: true,
+        speed: 8,
+        to: 1,
+      },
+      'idle-up': {
+        from: 12,
+        loop: true,
+        speed: 8,
+        to: 13,
+      },
+      'walk-down': {
+        from: 5,
+        loop: true,
+        speed: 8,
+        to: 3,
+      },
+      'walk-side': {
+        from: 8,
+        loop: true,
+        speed: 8,
+        to: 9,
+      },
+      'walk-up': {
+        from: 15,
+        loop: true,
+        speed: 8,
+        to: 17,
+      },
     },
-    'player',
-  ])
+    sliceX: 6,
+    sliceY: 3,
+  })
 
-  for (const layer of layers) {
-    if (layer.name === 'boundaries') {
-      for (const boundary of layer.objects) {
-        map.add([
-          k.area({
-            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
-          }),
-          k.body({ isStatic: true }),
-          k.pos(boundary.x, boundary.y),
-          boundary.name,
-        ])
+  k.setBackground(k.Color.fromHex('#000000'))
 
-        if (boundary.name) {
-          player.onCollide(boundary.name, () => {
-            if (dialogueData[boundary.name]) {
-              player.isInDialogue = true
-              showDialogue({
-                onClose: () => {
-                  player.isInDialogue = false
-                },
-                text: dialogueData[boundary.name],
-              })
-            }
-          })
+  k.scene('main', async () => {
+    const mapData = await (await fetch('./map.json')).json()
+    const layers = mapData.layers
+    const map = k.add([k.sprite('map'), k.pos(0, 0), k.scale(scaleFactor)])
+    const player = k.make([
+      k.sprite('spritesheet', {
+        anim: 'idle-down',
+      }),
+      k.area({
+        shape: new k.Rect(k.vec2(0, 3), 10, 10),
+      }),
+      k.body(),
+      k.anchor('center'),
+      k.pos(),
+      k.scale(scaleFactor),
+      {
+        direction: 'down',
+        isInDialogue: false,
+        speed: 300,
+      },
+      'player',
+    ])
+
+    for (const layer of layers) {
+      if (layer.name === 'boundaries') {
+        for (const boundary of layer.objects) {
+          map.add([
+            k.area({
+              shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+            }),
+            k.body({ isStatic: true }),
+            k.pos(boundary.x, boundary.y),
+            boundary.name,
+          ])
+
+          if (boundary.name) {
+            player.onCollide(boundary.name, () => {
+              if (dialogueData[boundary.name]) {
+                player.isInDialogue = true
+                showDialogue({
+                  onClose: () => {
+                    player.isInDialogue = false
+                  },
+                  text: dialogueData[boundary.name],
+                })
+              }
+            })
+          }
         }
+
+        continue
       }
 
-      continue
-    }
-
-    if (layer.name === 'spawnpoint') {
-      for (const entity of layer.objects) {
-        if (entity.name === 'Hero') {
-          player.pos = k.vec2(
-            (map.pos.x + entity.x) * scaleFactor,
-            (map.pos.y + entity.y) * scaleFactor
-          )
-          k.add(player)
-          continue
+      if (layer.name === 'spawnpoint') {
+        for (const entity of layer.objects) {
+          if (entity.name === 'Hero') {
+            player.pos = k.vec2(
+              (map.pos.x + entity.x) * scaleFactor,
+              (map.pos.y + entity.y) * scaleFactor
+            )
+            k.add(player)
+            continue
+          }
         }
       }
     }
-  }
 
-  setCameraScale(k)
-
-  k.onResize(() => {
     setCameraScale(k)
-  })
 
-  k.onUpdate(() => {
-    k.camPos(player.worldPos().x, player.worldPos().y - 100)
-  })
+    k.onResize(() => {
+      setCameraScale(k)
+    })
 
-  k.onMouseDown(mouseBtn => {
-    if (mouseBtn !== 'left' || player.isInDialogue) return
+    k.onUpdate(() => {
+      k.camPos(player.worldPos().x, player.worldPos().y - 100)
+    })
 
-    const worldMousePos = k.toWorld(k.mousePos())
-    player.moveTo(worldMousePos, player.speed)
-    const mouseAngle = player.pos.angle(worldMousePos)
-    const lowerBound = 50 /** @todo */
-    const upperBound = 125 /** @todo */
+    k.onMouseDown(mouseBtn => {
+      if (mouseBtn !== 'left' || player.isInDialogue) return
 
-    if (
-      mouseAngle > lowerBound &&
-      mouseAngle < upperBound &&
-      player.curAnim() !== 'walk-up'
-    ) {
-      player.play('walk-up')
-      player.direction = 'up'
-      return
-    }
+      const worldMousePos = k.toWorld(k.mousePos())
+      player.moveTo(worldMousePos, player.speed)
+      const mouseAngle = player.pos.angle(worldMousePos)
+      const lowerBound = 50 /** @todo */
+      const upperBound = 125 /** @todo */
 
-    if (
-      mouseAngle < -lowerBound &&
-      mouseAngle > -upperBound &&
-      player.curAnim() !== 'walk-down'
-    ) {
-      player.play('walk-down')
-      player.direction = 'down'
-      return
-    }
-
-    if (Math.abs(mouseAngle) > upperBound) {
-      player.flipX = false
-
-      if (player.curAnim() !== 'walk-side') player.play('walk-side')
-
-      player.direction = 'right'
-      return
-    }
-
-    if (Math.abs(mouseAngle) < lowerBound) {
-      player.flipX = true
-
-      if (player.curAnim() !== 'walk-side') player.play('walk-side')
-
-      player.direction = 'left'
-      return
-    }
-  })
-
-  function stopAnims() {
-    if (player.direction === 'down') {
-      player.play('idle-down')
-      return
-    }
-
-    if (player.direction === 'up') {
-      player.play('idle-up')
-      return
-    }
-
-    player.play('idle-side')
-  }
-
-  k.onMouseRelease(stopAnims)
-
-  k.onKeyRelease(() => {
-    stopAnims()
-  })
-
-  k.onKeyDown(() => {
-    const keyMap = [
-      k.isKeyDown('right'),
-      k.isKeyDown('left'),
-      k.isKeyDown('up'),
-      k.isKeyDown('down'),
-    ]
-    let keysPressed = 0
-
-    for (const key of keyMap) {
-      if (key) {
-        keysPressed++
+      if (
+        mouseAngle > lowerBound &&
+        mouseAngle < upperBound &&
+        player.curAnim() !== 'walk-up'
+      ) {
+        player.play('walk-up')
+        player.direction = 'up'
+        return
       }
+
+      if (
+        mouseAngle < -lowerBound &&
+        mouseAngle > -upperBound &&
+        player.curAnim() !== 'walk-down'
+      ) {
+        player.play('walk-down')
+        player.direction = 'down'
+        return
+      }
+
+      if (Math.abs(mouseAngle) > upperBound) {
+        player.flipX = false
+
+        if (player.curAnim() !== 'walk-side') player.play('walk-side')
+
+        player.direction = 'right'
+        return
+      }
+
+      if (Math.abs(mouseAngle) < lowerBound) {
+        player.flipX = true
+
+        if (player.curAnim() !== 'walk-side') player.play('walk-side')
+
+        player.direction = 'left'
+        return
+      }
+    })
+
+    function stopAnims() {
+      if (player.direction === 'down') {
+        player.play('idle-down')
+        return
+      }
+
+      if (player.direction === 'up') {
+        player.play('idle-up')
+        return
+      }
+
+      player.play('idle-side')
     }
 
-    if (keysPressed > 1) return
+    k.onMouseRelease(stopAnims)
 
-    if (player.isInDialogue) return
+    k.onKeyRelease(() => {
+      stopAnims()
+    })
 
-    if (keyMap[0]) {
-      player.flipX = false
+    k.onKeyDown(() => {
+      const keyMap = [
+        k.isKeyDown('right'),
+        k.isKeyDown('left'),
+        k.isKeyDown('up'),
+        k.isKeyDown('down'),
+      ]
+      let keysPressed = 0
 
-      if (player.curAnim() !== 'walk-side') player.play('walk-side')
+      for (const key of keyMap) {
+        if (key) {
+          keysPressed++
+        }
+      }
 
-      player.direction = 'right'
-      player.move(player.speed, 0)
-      return
-    }
+      if (keysPressed > 1) return
 
-    if (keyMap[1]) {
-      player.flipX = true
+      if (player.isInDialogue) return
 
-      if (player.curAnim() !== 'walk-side') player.play('walk-side')
+      if (keyMap[0]) {
+        player.flipX = false
 
-      player.direction = 'left'
-      player.move(-player.speed, 0)
-      return
-    }
+        if (player.curAnim() !== 'walk-side') player.play('walk-side')
 
-    if (keyMap[2]) {
-      if (player.curAnim() !== 'walk-up') player.play('walk-up')
+        player.direction = 'right'
+        player.move(player.speed, 0)
+        return
+      }
 
-      player.direction = 'up'
-      player.move(0, -player.speed)
-      return
-    }
+      if (keyMap[1]) {
+        player.flipX = true
 
-    if (keyMap[3]) {
-      if (player.curAnim() !== 'walk-down') player.play('walk-down')
+        if (player.curAnim() !== 'walk-side') player.play('walk-side')
 
-      player.direction = 'down'
-      player.move(0, player.speed)
-    }
+        player.direction = 'left'
+        player.move(-player.speed, 0)
+        return
+      }
+
+      if (keyMap[2]) {
+        if (player.curAnim() !== 'walk-up') player.play('walk-up')
+
+        player.direction = 'up'
+        player.move(0, -player.speed)
+        return
+      }
+
+      if (keyMap[3]) {
+        if (player.curAnim() !== 'walk-down') player.play('walk-down')
+
+        player.direction = 'down'
+        player.move(0, player.speed)
+      }
+    })
   })
-})
 
-k.go('main')
+  k.go('main')
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,45 +1,57 @@
-import type { KaboomCtx } from 'kaboom';
+import type { KaboomCtx } from 'kaboom'
+
+const isWebGlAvailable = (canvas: HTMLCanvasElement) =>
+  !!window.WebGLRenderingContext &&
+  (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
 
 const setCameraScale = (k: KaboomCtx) => {
-	const resizeFactor = k.width() / k.height();
+  const resizeFactor = k.width() / k.height()
 
-	if (resizeFactor < 1) return k.camScale(k.vec2(1));
+  if (resizeFactor < 1) return k.camScale(k.vec2(1))
 
-	k.camScale(k.vec2(1.5));
-};
+  k.camScale(k.vec2(1.5))
+}
 
-const showDialogue = ({ onClose, text }: { onClose: VoidFunction; text: string }) => {
-	const closeButton = document.getElementById('close') as HTMLButtonElement;
-	let currentText = '';
-	const dialogue = document.getElementById('dialogue') as HTMLDivElement;
-	const dialogueUI = document.getElementById('textbox-container') as HTMLDivElement;
-	let index = 0;
-	dialogueUI.style.display = 'block';
+const showDialogue = ({
+  onClose,
+  text,
+}: {
+  onClose: VoidFunction
+  text: string
+}) => {
+  const closeButton = document.getElementById('close') as HTMLButtonElement
+  let currentText = ''
+  const dialogue = document.getElementById('dialogue') as HTMLDivElement
+  const dialogueUI = document.getElementById(
+    'textbox-container'
+  ) as HTMLDivElement
+  let index = 0
+  dialogueUI.style.display = 'block'
 
-	const intervalRef = setInterval(() => {
-		if (index < text?.length) {
-			currentText += text[index];
-			dialogue.innerHTML = currentText;
-			index++;
-			return;
-		}
+  const intervalRef = setInterval(() => {
+    if (index < text?.length) {
+      currentText += text[index]
+      dialogue.innerHTML = currentText
+      index++
+      return
+    }
 
-		clearInterval(intervalRef);
-	}, 1);
+    clearInterval(intervalRef)
+  }, 1)
 
-	const onButtonClick = () => {
-		onClose();
-		dialogueUI.style.display = 'none';
-		dialogue.innerHTML = '';
-		clearInterval(intervalRef);
-		closeButton.removeEventListener('click', onButtonClick);
-	};
+  const onButtonClick = () => {
+    onClose()
+    dialogueUI.style.display = 'none'
+    dialogue.innerHTML = ''
+    clearInterval(intervalRef)
+    closeButton.removeEventListener('click', onButtonClick)
+  }
 
-	closeButton.addEventListener('click', onButtonClick);
+  closeButton.addEventListener('click', onButtonClick)
 
-	addEventListener('keypress', (key) => {
-		if (key.code === 'Enter') closeButton.click();
-	});
-};
+  addEventListener('keypress', key => {
+    if (key.code === 'Enter') closeButton.click()
+  })
+}
 
-export { setCameraScale, showDialogue };
+export { isWebGlAvailable, setCameraScale, showDialogue }
